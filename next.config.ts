e0f4e1f81ff7e42c24b2ded9dd72280d71b430b7
Ahleeryan <ahleeryan@hotmail.com>
2026-03-19 04:11:06 -0500

    pages test

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enables static exports
+  basePath: '/Paper-Trails';
+  images: {
+    unoptimized: true, // Required for static export
+  },
   reactCompiler: true,
 };
 
 
+
+module.exports = nextConfig;
 
 
 export default nextConfig;
