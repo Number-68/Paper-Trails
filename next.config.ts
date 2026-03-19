bd08e528487a4f8a55191c77f0d1601d90cf13fe
Ahleeryan <ahleeryan@hotmail.com>
2026-03-19 04:17:46 -0500

    add github pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Enables static exports
-  basePath: '/Paper-Trails';
+  basePath: '/Paper-Trails',
   images: {
     unoptimized: true, // Required for static export
   },
