a69b0b7f28304ce7e5f771313f1bc862beff2cab
Ahleeryan <ahleeryan@hotmail.com>
2026-03-28 17:43:50 -0500

    begin portfolio page

---
+++ constants/globalLinks.ts
@@ -0,0 +1,14 @@
+
+
+export const portfolioLinks = [
+    "https://github.com/Number-68/Paper-Trails",
+    "https://github.com/Number-68/Audio-Web",
+    "https://github.com/Number-68/Biy-ae",
+    "https://github.com/Number-68/sloth-breaker",
+    "https://github.com/Number-68/Token-Pouch"
+];
+
+
+export const githubLink = "https://github.com/Number-68";
+
+export const linkedInLink = "none yet";
