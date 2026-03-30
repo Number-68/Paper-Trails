2af4cfb4ca5542611d8fb2aac7c03475d6fc834f
Ahleeryan <ahleeryan@hotmail.com>
2026-03-30 17:27:24 -0500

    Updates

---
+++ _next/static/tcMPw7uOcUKy0TxzEIowp/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Paper-Trails//_next/:path+",
+        "destination": "/Paper-Trails/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
