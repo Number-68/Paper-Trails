41f9a978f0346f0a6b8a6d4dc0f7fbf9b1a2ae01
Ahleeryan <ahleeryan@hotmail.com>
2026-04-03 10:38:41 -0500

    prehook test

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,25 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
-
-
-
-
-export default nextConfig;
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   images: { unoptimized: true },
-//   basePath: '/Paper-Trails',
-//   assetPrefix: '/Paper-Trails/',
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
 // };
 
-// module.exports = nextConfig;
+
+
+
+// export default nextConfig;
+
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js image optimization
+  },
+  basePath: isProd ? "/Paper-Trails" : "",
+  assetPrefix: isProd ? "/Paper-Trails/" : "",
+};
+
+module.exports = nextConfig;
